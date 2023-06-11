1e50fe4b12043e599e0859ce1003eb72fac10fba
Delarram <tunyanaing960@gmail.com>
2023-06-11 16:16:43 +0630

    added nature model

---
+++ lib/page/shimmer_page.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+import '../model/nature.dart';
+
+class ShimmerPage extends StatefulWidget {
+  const ShimmerPage({Key? key}) : super(key: key);
+
+  @override
+  State<ShimmerPage> createState() => _ShimmerPageState();
+}
+
+class _ShimmerPageState extends State<ShimmerPage> {
+  List<Nature> natureList = [
+    Nature("Sea View", "Sea view resort",
+        "https://images.unsplash.com/photo-1589779677460-a15b5b5790ce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80"),
+    Nature("Awesome Nature", "Orange color fish with attractive look & fill",
+        "https://images.unsplash.com/photo-1520301255226-bf5f144451c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=873&q=80"),
+    Nature("Yellow Fish", "Yellow Fish in blue water",
+        "https://images.unsplash.com/photo-1504472478235-9bc48ba4d60f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80"),
+    Nature("Orange Fish", "Orange Fish in blue water",
+        "https://images.unsplash.com/photo-1535591273668-578e31182c4f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"),
+    Nature("Nature 1", "Nice tree in the nature",
+        "https://images.unsplash.com/photo-1624964562918-0b8cf87deee6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"),
+    Nature("Sea Nature", "Nice beach nature",
+        "https://images.unsplash.com/photo-1597499216184-b56bf75f84c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"),
+    Nature("Awesome Beach", "Beach with trees",
+        "https://images.unsplash.com/photo-1509057095372-215f8cc53d1a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=874&q=80"),
+    Nature("Blue Nature", "Beach with blue water",
+        "https://images.unsplash.com/photo-1558102822-da570eb113ed?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"),
+    Nature("Sea Nature 2", "Awesome beach with trees",
+        "https://images.unsplash.com/photo-1545579133-99bb5ab189bd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Shimmer Loading"),
+      ),
+      body: ListView(
+        shrinkWrap: true,
+        children: natureList.map((Nature nature) {
+          return Container(
+            margin: const EdgeInsets.only(left: 20, right: 20, top: 10),
+            height: 70.0,
+
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
