b7a28edcd2c0ee862bb9b85c4e18a08689d09c44
Delarram <tunyanaing960@gmail.com>
2023-08-03 16:10:05 +0630

    added aboutpage

---
+++ lib/lang/about_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import 'language_const.dart';
+
+
+class AboutPage extends StatefulWidget {
+  const AboutPage({Key? key}) : super(key: key);
+
+  @override
+  _AboutPageState createState() => _AboutPageState();
+}
+
+class _AboutPageState extends State<AboutPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(translation(context).home),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(20),
+        child: Center(
+          child: Text(translation(context).home),
+        ),
+      ),
+    );
+  }
+}
